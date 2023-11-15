3bf0ef0ea9db0125e5808d5fe4146c6ff92e30c0
Gaetano Vallarelli <biroplane@users.noreply.github.com>
2023-11-15 16:09:36 +0100

    Removes Prismic. Adds Netlify CMS

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+useHead({
+  title: "Ciao",
+  meta: [
+    {
+      name: "description",
+      content: "Mondo",
+    },
+  ],
+});
+</script>
+
+<template>
+  <main class="bg-red-500">Ciao</main>
+</template>
